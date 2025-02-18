7b9aa9a920852821de49320ecfa10e7986cdcee2
samiullah6799 <samiullah6799@gmail.com>
2025-02-18 12:10:25 +0500

    seventh commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Hello, World!</h1>"
+    return "MLOps SE(A)"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5050)
